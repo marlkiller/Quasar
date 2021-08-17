e24eb4af79ab8c95ddd22dccf1a4e5df0445b641
marlkiller <marlkiller@voidm.com>
2021-08-17 16:32:20 +0800

    git ignore

---
+++ Quasar.Common.Tests/CustomTest/Custom.Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static System.IO.DriveInfo;
+
+namespace Quasar.Common.Tests.CustomTest
+{
+    [TestClass]
+    public class FileHelperTests
+    {
+        
+        [TestMethod, TestCategory("Custom")]
+        public void CompressionTest()
+        {
+            var driveInfos = GetDrives().Where(d => d.IsReady).ToArray();
+            foreach (var driveInfo in driveInfos)
+            {
+                Console.Write(driveInfo.Name);
+            }
+        }
+      
+    }
+}
